43eb30452404fe615dad1e715a1f115bbcb1fdb6
Siva <sivas.online@gmail.com>
2014-01-03 23:40:40 -0500

    Remove python files

--- Test.py
+++
@@ -1,7 +0,0 @@
-import Tkinter
-from Tkinter import Tcl
-root = Tcl()
-print "This is root",root
-
-
-root.eval("puts Siva")
--- psutil_test.py
+++
@@ -1,6 +0,0 @@
-import psutil
-#print psutil.phymem_usage()
-import Tkinter
-root = Tkinter.Tcl()
-dir(root)
-root.eval("puts Hello World !")
